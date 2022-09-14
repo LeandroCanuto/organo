9d89df7a99547b9941df10090db44c82dfc7b6cd
LeandroCanuto <leandrocanutto@hotmail.com>
2022-09-14 15:46:14 -0300

    cabecalho

--- src/componentes/Banner/Banner.js
+++ src/componentes/Banner/Banner.js
@@ -7,7 +7,7 @@ function Banner ()
     return(
     <header className="banner">
         <img src="imagens/banner.png" alt="Banner de cabeçalho"></img>
-        </header>
+    </header>
         )  
  
     
